033b88e0ecf6e133f2bf05b74501584c7d90dc7a
Mitchell Thomas <mthomas60@my.wctc.edu>
2024-10-08 13:56:18 -0500

    makes display method virtual

--- Character.cs
+++ Character.cs
@@ -2,7 +2,7 @@ public abstract class Character{
     public UInt64 ID { get; set; }
     public string Name { get; set; } = "";
     public string Description { get; set; } = "";
-    public string Display(){
+    public virtual string Display(){
         return $"Id: {ID}\nName: {Name}\nDescription: {Description}";
     }
 }
